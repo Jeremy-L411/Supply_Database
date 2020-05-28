415e179777121b9283f077d669903fa5514e7cb7
Jeremy <36969209+Jeremy-L411@users.noreply.github.com>
2020-05-27 21:03:08 -0600

    complete rewrite
    Rewrote the program, adjusted loops and errors

--- SQL_Supply_Builder.py
+++ SQL_Supply_Builder.py
@@ -7,10 +7,11 @@ import os
 import sys
 
 """
-    A Program that creates tables, inserts data from .csv files, adds single lines, delete
-    single lines without having to use SQL code. This program also works with EXP_Dates, but only if the 
-    table names are in Box0, Box1 etc. format.
-    """
+    A program that can create a database if it does not already exist, creates tables with a specific format, 
+    ability to import .csv files into tables if the formatting is the same as the created table, add single rows, 
+    delete single rows and adjust quantities without having to use any SQL statements. Program was created as an
+    inventory management for supplies. Columns: Category, Product, Packaging, Volume_size, Quantity, And Expiration date 
+"""
 
 
 def create_connection(db_file):
@@ -27,82 +28,79 @@ def create_connection(db_file):
 
 
 def create_new_db(path):
-    """Creates a new database if one is not already created"""
+    """Creates a new database at given location"""
+
     conn = sqlite3.connect(path)
     conn.commit()
     conn.close()
 
 
-def find_db():
-    """Searches directory where program is located for db, if none
-        found asks for location of the db and returns location.
-        if answer no exits program. Later implementation will write
-        db location to a file and read read from file first, then
-        current directory and then ask, if no db found will ask if
-        wanted to create a db"""
+def show_all_data(conn, table):
+    """Displays all data in a given table, columns wrap, difficult to read"""
 
-    try:
-        with os.scandir(os.getcwd()) as it:
-            for entry in it:
-                if entry.name.endswith('.db'):
-                    db = entry.name
-                    break
-            else:
-                raise FileExistsError
-
-    except FileExistsError:
-        print("No data base found here")
-        new_db = input("Is there another location (Yes, No)\n")
-        if new_db.lower() == ('no' or 'new' or 'create'):
-            create_db = input("Create new db or exit?\n")
-            if create_db.lower() == ('new' or 'create'):
-                db_name = input('DB path and Name?\n'
-                                'ex: /Users/user/Desktop/test.db\n')
-                create_new_db(db_name)
-                db = create_connection(db_name)
-            else:
-                sys.exit()
-        else:
-            db = input("Where is the DB?\n")
-    return db
-
-
-def print_data(conn, table):
-    """Returns all data in a table
-        Future, make own categories or preset ones for this program
-        """
-
+    pd.set_option("display.max_rows", None)
+    pd.set_option("display.max_columns", None)
     c = conn.cursor()
-    c.execute("""Select * FROM {}""".format(table))
+    c.execute("""SELECT * FROM '{0}'""".format(table))
     df = DataFrame(c.fetchall(), columns=['Category', 'Product', 'Packaging', 'Volume_Size', 'Quantity', 'EXP'])
     return print(df)
 
 
-def show_tables(conn):
-    """Shows all tables for connected database"""
+def print_data(conn, table):
+    """Displays all rows in the given table, only shows Product, Quantity, and EXP"""
 
+    pd.set_option('display.max_rows', None)
     c = conn.cursor()
-    c.execute("""SELECT name FROM sqlite_master
-        WHERE type='table'
-        ORDER BY name;""")
-    df = DataFrame(c.fetchall(), columns=['name'])
+    c.execute("""SELECT Product, QUANTITY, EXP FROM '{0}'""".format(table))
+    df = DataFrame(c.fetchall(), columns=["Product", "Quantity", "EXP"])
     return print(df)
 
 
-def make_table(conn, table_name):
-    """Makes new tables in the database with pre-established columns"""
+def show_tables(conn):
+    """Displays all tables in the database"""
 
     c = conn.cursor()
-    c.execute("""CREATE TABLE '{}' (
-        "Category"	TEXT NOT NULL,
-        "Product"	TEXT NOT NULL,
-        "Packaging"	TEXT,
-        "Volume_Size"	TEXT,
-        "Quantity"	INTEGER,
-        "EXP"	INTEGER DEFAULT 'NA'
-        )""".format(table_name))
+    c.execute("""SELECT name FROM sqlite_master
+                WHERE type='table'
+                ORDER BY name""")
+    df = DataFrame(c.fetchall(), columns=["name"])
+    return print(df)
+
+
+def check_tables(conn, table):
+    """Checks to see if a table is in the database, if not returns error"""
+
+    c = conn.cursor()
+    c.execute("""SELECT name FROM "main".sqlite_master where name = '{0}'""".format(table))
+    if len(c.fetchall()) > 0:
+        return True
+    else:
+        raise ValueError
+
+
+def make_table(conn, table_name):
+    """Creates a new table with the columns predefined for the inventory management"""
+
+    c = conn.cursor()
+    c.execute("""CREATE TABLE IF NOT EXISTS {0} (
+                "Category" TEXT NOT NULL,
+                "Product" TEXT NOT NULL, 
+                "Packaging" TEXT,
+                "Volume_Size" TEXT, 
+                "Quantity" INTEGER,
+                "EXP" INTEGER)
+                """.format(table_name))
     conn.commit()
     print_data(conn, table_name)
+
+
+def drop_table(conn, table):
+    """Drops a selected table"""
+
+    c = conn.cursor()
+    c.execute("""DROP TABLE {0}""".format(table))
+    conn.commit()
+    show_tables(conn)
 
 
 def csv_insert(conn, file, table):
@@ -122,7 +120,7 @@ def csv_insert(conn, file, table):
 
 
 def add_item(conn, table):
-    """Adds a single item to a single table"""
+    """Adds a single item into a given table"""
 
     c = conn.cursor()
     category = input('Category? ')
@@ -132,92 +130,220 @@ def add_item(conn, table):
     quantity = input('Quantity? ')
     exp = input('Expiration date in Year, Month, Day? ')
     c.execute("""Insert into '{0}' (CATEGORY, PRODUCT, PACKAGING, VOLUME_SIZE, QUANTITY, EXP)
-        VALUES ('{1}', '{2}', '{3}', '{4}', '{5}', '{6}')""".format(table, category, product, packaging,
-                                                                    volume_size, quantity, exp))
+            VALUES ('{1}', '{2}', '{3}', '{4}', '{5}', '{6}')""".format(table, category, product, packaging,
+                                                                        volume_size, quantity, exp))
 
     conn.commit()
     print_data(conn, table)
 
 
 def remove_item(conn, table, item):
-    """Removes a single item from a table"""
+    """Removes a single item from a table, using the Product column"""
 
     c = conn.cursor()
-    c.execute("""DELETE FROM {0} WHERE PRODUCT = '{1}'""".format(table, item))
+    c.execute("""DELETE FROM '{0}' WHERE PRODUCT = '{1}'""".format(table, item))
 
     conn.commit()
     print_data(conn, table)
 
 
-def main():
+def adjust_quantity(conn, table, item, quantity):
+    """Updates the Quantity of a given product in a given table"""
 
-    session = 'open'
-    while session == 'open':
+    c = conn.cursor()
+    c.execute("""UPDATE {0} SET Quantity = '{1}' WHERE Product = '{2}' """.format(table, quantity, item))
+    conn.commit()
+    print_data(conn, table)
+
+
+def find_db():
+    """First scans directory where the program resides, if none is found asks for another location. If no
+        other database is present one is able to be created"""
+
+    db = None
+    while not db:
         try:
-            test = input("What would you like to do?\n"
-                         "Insert file into table?\n"
-                         "Make a new table?\n"
-                         "Remove item?\n"
-                         "Insert single item?\n")
-
-            conn = create_connection(database)
-
-            if ("insert" and "file") in test.lower():
-                complete = 'no'
-                while complete == 'no':
-                    file_location = input("Where is the file?\n")
-                    show_tables(conn)
-                    imp_table = input("Which table?\n")
-
-                    csv_insert(conn, file_location, imp_table)
-                    complete = input("Are you finished?\n").lower()
-
-            elif ("make" and "table") in test.lower():
-                new_table = 'no'
-                while new_table == 'no':
-                    print("Current Tables:")
-                    show_tables(conn)
-                    table_name = input("What table name?\n")
-
-                    make_table(conn, table_name)
-                    new_table = input("Finished adding tables?\n").lower()
-
-            elif "remove" in test.lower():
-                remove = 'no'
-                while remove == 'no':
-                    show_tables(conn)
-                    table = input('What table is this in?\n')
-                    print_data(conn, table)
-                    item = input('What product are we removing?\n')
-
-                    remove_item(conn, table, item)
-                    remove = input('Finished removing items?\n').lower()
-
-            elif ("insert" or "single") in test.lower():
-                item_add = 'no'
-                while item_add == 'no':
-                    show_tables(conn)
-                    table = input('What table would you like to add to?\n')
+            with os.scandir(os.getcwd()) as it:
+                for entry in it:
+                    if entry.name.endswith('.db'):
+                        db = os.path.join(os.getcwd(), entry.name)
+                        return db
+                else:
+                    raise FileExistsError
+        except FileExistsError:
+            print("No database found here")
+            new_db = input("Is there another location for \n"
+                           "existing database? (Yes, No)\n"
+                           "or exit\n").lower()
+            if "no" in new_db:
+                create_db = input("Database path and name?\n"
+                                  "ex: /Users/user/Desktop/test.db\n")
+                create_new_db(create_db)
+                db = create_connection(create_db)
+                return db
+            elif "yes" in new_db:
+                existing_db = input("Path of existing Database?\n")
+                if existing_db.endswith(".db") and os.path.exists(existing_db):
+                    db = existing_db
+                    return db
+            elif "exit" in new_db:
+                sys.exit()
+            break
 
 
-                    add_item(conn, table)
-                    item_add = input('Finished adding?\n').lower()
+def __main__():
 
-            else:
-                raise OperationalError
+    database = None
+    while not database:
+        database = find_db()
+        if database:
+            break
+    session = True
+    conn = create_connection(database)
 
-            conn.commit()
-            conn.close()
-            session = 'closed'
-        except OperationalError:
-            print("lets try that again!\n")
+    while session:
+        try:
+            print("You are working in {} Database!".format(database.rsplit('/', 1)[1]))
+            process = input("What would you like to do?\n"
+                            "* Note, phrases must be typed exactly *\n\n"
+                            "Make a new table?\n"
+                            "Insert file into table?\n"
+                            "Insert single item?\n"
+                            "Adjust Quantity?\n"
+                            "Remove item?\n"
+                            "Show all tables?\n"
+                            "Print table contents?\n"
+                            "Remove table?\n" 
+                            "or exit?\n").lower()
+
+            if process == "make a new table":
+                table_loop = True
+                while table_loop:
+                    try:
+                        print("Current Tables:")
+                        show_tables(conn)
+                        new_table = input("What is the name of the new table?\n")
+                        make_table(conn, new_table)
+                        table_loop = input("Finished adding tables?\n").lower()
+                        if "yes" in table_loop:
+                            break
+
+                    except OperationalError:
+                        print("Something went wrong, lets try that again!")
+
+            if process == "remove table":
+                remove_table_loop = True
+                while remove_table_loop:
+                    try:
+                        show_tables(conn)
+                        rmv_table = input("What table are we removing?\n")
+                        if check_tables(conn, rmv_table):
+                            drop_table(conn, rmv_table)
+                            remove_table_loop = input("Finished removing tables?\n").lower()
+                            if "yes" in remove_table_loop:
+                                break
+
+                    except OperationalError:
+                        print("Something went wrong, lets try that again!")
+
+            if process == "insert file into table":
+                insert_loop = True
+                while insert_loop:
+                    try:
+                        file_loc = input("What is the path of the .csv file?\n")
+                        if (file_loc.endswith(".csv") and os.path.exists(file_loc)):
+                            show_tables(conn)
+                            tbl = input("Which table do you want to import file to?\n")
+                            if check_tables(conn, tbl):
+                                csv_insert(conn, file_loc, tbl)
+                                insert_loop = input("Finished importing files?\n").lower()
+                                if "yes" in insert_loop:
+                                    break
+                        else:
+                            raise FileNotFoundError
+
+                    except FileNotFoundError:
+                        print("File not found or not .csv\n"
+                              "Please try again!\n")
+                    except OperationalError:
+                        print("Table not found in Database, please try again!")
+
+            if process == "insert single item":
+                single_add_loop = True
+                while single_add_loop:
+                    try:
+                        show_tables(conn)
+                        insert_item_table = input("What Table would you like to add to?\n")
+                        if check_tables(conn, insert_item_table):
+                            add_item(conn, insert_item_table)
+                            single_add_loop = input("Finished add items?\n").lower()
+                            if "yes" in single_add_loop:
+                                break
+
+                    except OperationalError:
+                        print("Table not found in Database, or value incorrect type.\n"
+                              "Please try again!")
+                    except ValueError:
+                        print("Table not in database")
+
+            if process == "adjust quantity":
+                adjust_loop = True
+                while adjust_loop:
+                    try:
+                        show_tables(conn)
+                        adj_table = input("What table is the product in?\n")
+                        print_data(conn, adj_table)
+                        adj_item = input("What item are we adjusting?\n")
+                        adj_quantity = input("What is the new quantity?\n")
+                        if check_tables(conn, adj_table):
+                            adjust_quantity(conn, adj_table, adj_item, adj_quantity)
+                            adjust_loop = input("Finished adjusting items?\n").lower()
+                            if 'yes' in adjust_loop:
+                                adjust_loop = False
+                                break
+
+                    except OperationalError:
+                        print("Something went wrong, please try again!")
+
+            if process == "remove item":
+                rmv_itm_loop = True
+                while rmv_itm_loop:
+                    try:
+                        show_tables(conn)
+                        rmv_itm_table = input("What table is the item in?\n")
+                        print_data(conn, rmv_itm_table)
+                        item = input("What item are we removing?\n"
+                                     "* (If item is not in list no error will show) *\n")
+                        if check_tables(conn, rmv_itm_table):
+                            remove_item(conn, rmv_itm_table, item)
+                            rmv_itm_loop = input("Finished deleting items?\n")
+                            if "yes" in rmv_itm_loop:
+                                break
+                    except OperationalError:
+                        print("Table not found in Database, or value incorrect type.\n"
+                              "Please try again!")
+                    except ValueError:
+                        print("Item not in table")
+
+            if process == "show all tables":
+                show_tables(conn)
+
+            if process == "print table contents":
+                show_tables(conn)
+                print_table = input("What table do you want to see?\n")
+                show_all_data(conn, print_table)
+
+            if process == "exit":
+                conn.close()
+                sys.exit()
+
+            session = input("Are you finished with Database {}?\n".format(database.rsplit('/', 1)[1]))
+            if 'yes' in session:
+                break
+
+        except (FileNotFoundError, FileExistsError):
+            print("Something went wrong, please try again!")
 
 
-if __name__ == '__main__':
-
-    database = find_db()
-
-    project = "no"
-    while project == "no":
-        main()
-        project = input("are you finished with the database?\nYes, No?\n").lower()
+if __name__ == "__main__":
+    __main__()
